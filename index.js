3ba78a014f21a2daba5016fb8d3a5e9802f43a7e
Alexander Ozaveshe Adogah <alexanderozavesheadogah@gmail.com>
2026-04-06 20:11:53 +0000

    Final stable backend fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Default route
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
